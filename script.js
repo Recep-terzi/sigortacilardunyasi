2000523b511c127c10e5c996c52cecfa0a890472
recep-terzi <recepterzi-67@hotmail.com>
2024-06-03 17:26:48 +0300

    update

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 
 function changeColor(element) {
-    // Seçili olan öğenin sınıfını "selected" olarak ayarla
-    // ve diğer öğelerin sınıfını temizle
     var items = document.querySelectorAll('.bar__left p');
     items.forEach(function(item) {
         if (item === element) {
