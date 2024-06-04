eb0f504f4b33535b3ce6d41dab67269027896123
recep-terzi <recepterzi-67@hotmail.com>
2024-06-04 13:32:47 +0300

    update

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ function changeColor(element) {
         }
     });
 }
+
+document.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 0) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
